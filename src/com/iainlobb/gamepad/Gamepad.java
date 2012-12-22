c797e53ed25ea94e8df7cfcee0be1385f3f076ed
Michael Heuer <heuermh@acm.org>
2012-12-21 22:01:48 -0600

    update version to 2.0

--- src/com/iainlobb/gamepad/Gamepad.java
+++ src/com/iainlobb/gamepad/Gamepad.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2010 Iain Lobb, Michael Heuer
+Copyright (c) 2010-2012 Iain Lobb, Michael Heuer
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
