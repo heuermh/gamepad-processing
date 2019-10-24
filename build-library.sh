38bc007936988ec56553c1711cb6a8f1b2dfdd6e
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:52:54 -0500

    update processing-core dependency version to 3.5.3

--- build-library.sh
+++ build-library.sh
@@ -8,6 +8,6 @@ cp library.properties gamepad
 cp -R src gamepad
 cp -R examples gamepad
 cd src
-javac -source 1.8 -target 1.8 -classpath ../lib/processing-core-3.3.6.jar com/iainlobb/gamepad/*.java
+javac -source 1.8 -target 1.8 -classpath ../lib/processing-core-3.5.3.jar com/iainlobb/gamepad/*.java
 jar cvf ../gamepad/library/gamepad.jar com/iainlobb/gamepad/*.class
 cd ..
